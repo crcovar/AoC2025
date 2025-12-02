05dc2be985aec4a32e09a28c7b1c7c03c0e481d7
Charles Covar <ccovar@flexgen.com>
2025-12-02 13:26:34 -0500

    feat: Complete Day 2

--- Day2/Day2.hs
+++ Day2/Day2.hs
@@ -8,6 +8,12 @@ wordsOn p s = case dropWhile p s of
     where
       (w, s'') = break p s'
 
+group' :: Int -> [a] -> [[a]]
+group' _ [] = []
+group' n l
+  | n > 0 = (take n l):(group' n (drop n l))
+  | otherwise = [l]
+
 parseRange :: String -> [Integer]
 parseRange r =
   let (x : xs : _) = wordsOn (== '-') r
@@ -15,19 +21,34 @@ parseRange r =
       end = read xs :: Integer
    in [start .. end]
 
--- isRepeating :: Integer -> Bool
--- isRepeating i =
---   let s = show i
---       xs = tail s ++ s
---    in case s `elemIndex` xs of
---       Nothing -> False
---       Just i' -> i' < len s
+isRepeatingTwice :: Integer -> Bool
+isRepeatingTwice i
+  | odd (length s) = False
+  | x == y = True
+  | otherwise = False
+  where s = show i
+        (x,y) = splitAt (length s `div` 2) s
+
+isRepeating :: Int -> String -> Bool
+isRepeating _ [] = True
+isRepeating j s
+  | j > h = False
+  | n == 0 = True
+  | otherwise = isRepeating (j+1) s
+  where
+        h = length s `div` 2
+        (x:xs) = group' j s
+        n = length $ filter (/=x) xs
+
+part1 :: String -> Integer
+part1 = sum . concat . map (filter isRepeatingTwice . parseRange) . wordsOn (== ',')
+
+part2 :: String -> Integer
+part2 = sum . concat . map (filter (isRepeating 1 . show) . parseRange) . wordsOn (== ',')
 
 main = do
-  file <- readFile "example.txt"
-  let ranges = map parseRange $ wordsOn (== ',') file
-  print $ ranges
+  file <- readFile "input.txt"
   putStrLn "Part 1"
-  print "Not yet solved"
+  print $ part1 file
   putStrLn "Part 2"
-  print "Not yet solved"
+  print $ part2 file
